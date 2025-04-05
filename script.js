6b89176c063e649c560ef16fe87553369daa3ae1
Nasser <s.nasser.hadi@gmail.com>
2025-04-05 23:06:01 +0330

    Get DatePicker value with EventListener

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
-function getGregorianDateDetails() {
-    const datePicker = document.getElementById('datePicker');
-    const dateValue = datePicker.value;
-    // Notice : G stands for of Gregorian Date
+
+// Notice : 
+// G stands for of Gregorian Date
+
+let dateValue;
+let Year_G = 0;
+let Month_G = 0;
+let Day_G = 0;
+
+document.querySelector('#datePicker').addEventListener('change', function (event) {
+
+    dateValue = event.target.value;
+
     if (dateValue) {
-        const date = new Date(dateValue);
-        const Year_G = date.getFullYear();
-        const month_G = date.getMonth() + 1; // Months are zero-based
-        const Day_G = date.getDate();
+        document.getElementById('dateDetails01').innerText = '';
 
-        document.getElementById('dateDetails01').innerText = `Year: ${Year_G} - Month: ${month_G} - Day: ${Day_G} `;
+        let date = new Date(dateValue);
+        Year_G = date.getFullYear();
+        Month_G = date.getMonth() + 1; // Months are zero-based
+        Day_G = date.getDate();
+    }
+    else {
+        document.getElementById('dateDetails01').innerText = 'Please select a date.';
+    }
+});
 
+
+function getGregorianDateDetails() {
+    if (dateValue) {
+        document.getElementById('dateDetails01').innerText = `Year: ${Year_G} - Month: ${Month_G} - Day: ${Day_G} `;
     } else {
         document.getElementById('dateDetails01').innerText = 'Please select a date.';
     }
