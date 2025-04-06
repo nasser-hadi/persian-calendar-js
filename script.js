e5895c958915fcd001ac7f1b68f8ad053089891b
Nasser <s.nasser.hadi@gmail.com>
2025-04-06 20:33:22 +0330

    Fixed an error when clearing the Date from the Input.

--- script.js
+++ script.js
@@ -17,21 +17,25 @@ document.querySelector('#datePicker').addEventListener('change', function (event
         Year_G = date.getFullYear();
         Month_G = date.getMonth() + 1; // Months are zero-based
         Day_G = date.getDate();
-
-        gregorianDateDetails(dateValue);
     }
-    else {
-        document.getElementById('year_G').innerText = 'Please select a date.';
-    }
+    gregorianDateDetails(dateValue);
 });
 
-
 function gregorianDateDetails(date2) {
+
+    let lable = document.getElementById('notice');
+   
     if (date2) {
-        document.getElementById('year_G').innerText= `Year : ${Year_G} `;
-        document.getElementById('month_G').innerText= `Month : ${Month_G.toString().padStart(2,'0')} `;
-        document.getElementById('day_G').innerText= `Day : ${Day_G.toString().padStart(2,'0')} `;
+        document.getElementById('year_G').innerText = `Year : ${Year_G} `;
+        document.getElementById('month_G').innerText = `Month : ${Month_G.toString().padStart(2, '0')} `;
+        document.getElementById('day_G').innerText = `Day : ${Day_G.toString().padStart(2, '0')} `;
+
+        lable.classList.remove('text-danger');
     } else {
-        document.getElementById('year_G').innerText = 'Please select a date.';
+        document.getElementById('year_G').innerText = `Year : `;
+        document.getElementById('month_G').innerText = `Month : `;
+        document.getElementById('day_G').innerText = `Day : `;
+        
+        lable.classList.add('text-danger');
     }
 }
